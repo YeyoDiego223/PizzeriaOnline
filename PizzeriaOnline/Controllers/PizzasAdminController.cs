15126ce088aaf698e636804c0a748bb39e861ab2
YeyoDiego223 <floresdieg905@gmail.com>
2025-06-26 21:52:31 -0600

    feat: Implementa la vista de lista (Read) para CRUD de Pizzas en Admin

---
+++ PizzeriaOnline/Controllers/PizzasAdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using PizzeriaOnline.Data;
+using PizzeriaOnline.Models;
+
+namespace PizzeriaOnline.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PizzasAdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public PizzasAdminController(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var listadePizzas = _context.Pizzas.ToList();
+            return View(listadePizzas);
+        }
+    }
+}
