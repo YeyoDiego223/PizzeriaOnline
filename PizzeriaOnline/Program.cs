0e8fd8d6425f4b8fb0545a330bda3feeff881797
YeyoDiego223 <floresdieg905@gmail.com>
2025-09-12 20:34:36 -0600

    Elimina las migraciones

--- PizzeriaOnline/Program.cs
+++ PizzeriaOnline/Program.cs
@@ -55,7 +55,8 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     try
     {
-        await SeedData.Initialize(services);
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
     }
     catch (Exception ex)
     {
